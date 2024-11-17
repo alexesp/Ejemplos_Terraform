9e829751c4fd210c28b3e4944dd37b23fc9f040b
Dev_Ops <alexizaes@hotmail.com>
2024-11-17 06:26:38 -0500

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.75"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-north-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-090abff6ae1141d7d"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Example Instance"
+  }
+}
